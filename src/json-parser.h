b858062ceae597e5977fe835ce965c5128c9eb36
Maxim Shamber <max.shamber@gmail.com>
2020-11-10 16:28:00 +0300

    Remove unnecessary comments

--- src/json-parser.h
+++ src/json-parser.h
@@ -17,8 +17,8 @@ namespace gr {
 
 	graph_t importGraph(const boost::property_tree::ptree& pt, graph_t& graph, vertexMap_t& vertexMap, edgeMap_t& edgeMap)
 	{
-		graph.m_property->name = pt.get<std::string>("name");
-		graph.m_property->idx = pt.get<uint32_t>("idx");
+		graph.m_property->name = pt.get<std::string>("name"); // name
+		graph.m_property->idx = pt.get<uint32_t>("idx"); // idx
 
 		for (const auto& point : pt.get_child("points"))
 		{
@@ -30,12 +30,6 @@ namespace gr {
 				point.second.get_optional<uint32_t>("post_idx").get_value_or(uint32_t_max), // post_idx
 				0xFF0000 // color
 				}, graph);
-			
-
-			//boost::put(boost::vertex_index, graph, v, idx);
-			//boost::put(boost::vertex_name, graph, v, std::to_string(idx));
-			//boost::put(vertex_post_index, graph, v, point.second.get_optional<uint32_t>("post_idx").get_value_or(uint32_t_max));
-			//boost::put(boost::vertex_color, graph, v, 0xFF0000);
 
 			vertexMap[idx] = v;
 		}
@@ -47,16 +41,10 @@ namespace gr {
 
 			edge_t e = boost::add_edge(vertexMap[pts[0]], vertexMap[pts[1]], EdgeProps
 				{
-				std::to_string(idx), //name
-				line.second.get<double>("length"), //length
-				0xFF00FF //color
+				std::to_string(idx), // name
+				line.second.get<double>("length"), // length
+				0xFF00FF // color
 				}, graph).first;
-			
-
-			//boost::put(boost::edge_index, graph, e, idx);
-			//boost::put(boost::edge_name, graph, e, std::to_string(idx));
-			//boost::put(boost::edge_weight, graph, e, line.second.get<double>("length"));
-			//boost::put(boost::edge_color, graph, e, 0xFF00FF);
 
 			edgeMap[idx] = e;
 		}
