46ac8c5f6470dfcf2a798d33ed787876daa1b3c1
Maxim Shamber <max.shamber@gmail.com>
2020-11-10 15:44:14 +0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,51 @@
+#include "json-parser.h"
+
+#include <boost/graph/graphviz.hpp>
+
+#include <iomanip>
+
+int main()
+{
+
+	gr::ptree pt;
+	boost::property_tree::read_json("small_graph.json", pt);
+
+	gr::graph_t graph;
+	gr::vertexMap_t vertexMap;
+	gr::edgeMap_t edgeMap;
+
+	gr::importGraph(pt, graph, vertexMap, edgeMap);
+
+	auto graph_vertices = boost::vertices(graph);
+
+	std::cout << "vertices:" << std::endl;
+	gr::vertex_iterator v, vend;
+	for (boost::tie(v, vend) = boost::vertices(graph); v != vend; ++v)
+	{
+		const gr::VertexProps& vertex = graph[*v];
+
+		std::cout << "id:" << *v
+			<< " name:" << vertex.name
+			<< " post_idx:" << vertex.post_idx
+			<< " color:" << vertex.color
+			<< std::endl;
+	}
+
+	std::cout << std::endl << "edges:" << std::endl;
+	gr::edge_iterator e, eend;
+	for (boost::tie(e, eend) = boost::edges(graph); e != eend; ++e)
+	{
+		const gr::EdgeProps& edge = graph[*e];
+
+		std::cout << "id:" << *e
+			<< " name:" << edge.name
+			<< " length:" << edge.length
+			<< " color:" << edge.color
+			<< std::endl;
+	}
+
+	std::cout << std::endl;
+	boost::write_graphviz(std::cout, graph);
+
+	return 0;
+}
---
+++ src/json-parser.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include "graph_t.h"
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <limits>
+
+namespace gr {
+
+	static constexpr uint32_t uint32_t_max = std::numeric_limits<uint32_t>::max();
+
+	graph_t importGraph(const boost::property_tree::ptree& pt, graph_t& graph, vertexMap_t& vertexMap, edgeMap_t& edgeMap)
+	{
+		
+
+		for (const auto& point : pt.get_child("points"))
+		{
+			uint32_t idx = point.second.get<uint32_t>("idx");
+
+			vertex_t v = boost::add_vertex(VertexProps
+				{
+				std::to_string(idx), // name
+				point.second.get_optional<uint32_t>("post_idx").get_value_or(uint32_t_max), // post_idx
+				0xFF0000 // color
+				}, graph);
+			
+
+			//boost::put(boost::vertex_index, graph, v, idx);
+			//boost::put(boost::vertex_name, graph, v, std::to_string(idx));
+			//boost::put(vertex_post_index, graph, v, point.second.get_optional<uint32_t>("post_idx").get_value_or(uint32_t_max));
+			//boost::put(boost::vertex_color, graph, v, 0xFF0000);
+
+			vertexMap[idx] = v;
+		}
+
+		for (const auto& line : pt.get_child("lines"))
+		{
+			uint32_t idx = line.second.get<uint32_t>("idx");
+			auto pts = as_vector<uint32_t>(line.second, "points");
+
+			edge_t e = boost::add_edge(vertexMap[pts[0]], vertexMap[pts[1]], EdgeProps
+				{
+				std::to_string(idx), //name
+				line.second.get<double>("length"), //length
+				0xFF00FF //color
+				}, graph).first;
+			
+
+			//boost::put(boost::edge_index, graph, e, idx);
+			//boost::put(boost::edge_name, graph, e, std::to_string(idx));
+			//boost::put(boost::edge_weight, graph, e, line.second.get<double>("length"));
+			//boost::put(boost::edge_color, graph, e, 0xFF00FF);
+
+			edgeMap[idx] = e;
+		}
+
+		return graph;
+	}
+
+} // namespace gr
