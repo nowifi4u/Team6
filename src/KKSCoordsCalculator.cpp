7b1076f9c508fffd48fd92c6350d5fb4d5818608
Maxim Shamber <max.shamber@gmail.com>
2020-11-23 14:26:59 +0300

    Incapsulate Graph and idmaps into GraphIdx

--- src/KKSCoordsCalculator.cpp
+++ src/KKSCoordsCalculator.cpp
@@ -1,6 +1,6 @@
 #include "KKSCoordsCalculator.h"
 
-void KKSCoordsCalculator::calculate(Graph& g, double topology_width, double topology_height, double unit_edge_length)
+void KKSCoordsCalculator::calculate(GraphIdx::Graph& g, double topology_width, double topology_height, double unit_edge_length)
 {
 	PositionVec position_vec(num_vertices(g));
 	PositionMap position(position_vec.begin(), get(boost::vertex_index, g));
@@ -12,7 +12,7 @@ void KKSCoordsCalculator::calculate(Graph& g, double topology_width, double topo
 	bool kamada = kamada_kawai_spring_layout(
 		g,
 		position,
-		boost::make_transform_value_property_map(to_double(), get(&EdgeProperties::length, g)),
+		boost::make_transform_value_property_map(to_double(), get(&GraphIdx::EdgeProperties::length, g)),
 		topo,
 		boost::edge_length(unit_edge_length),
 		KamadaKawaiDone()
