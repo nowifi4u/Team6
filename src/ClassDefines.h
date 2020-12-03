42c42d1d4c5ab4ea22e6208c8371328bc2ed5d13
Maxim Shamber <max.shamber@gmail.com>
2020-12-04 02:48:17 +0300

    Update ClassDefines.h

--- src/ClassDefines.h
+++ src/ClassDefines.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <boost/typeof/decltype.hpp>
+
 #define THIS (*this)
 #define super_type __super
 
-#define VIRTUAL_DESTRUCTOR(CLASS) \
+#define CLASS_VIRTUAL_DESTRUCTOR(CLASS) \
 virtual ~CLASS() {}
 
 #define CLASS_MAKE_STATIC(CLASS, ...) \
@@ -13,3 +15,15 @@ CLASS ( CLASS __VA_ARGS__ & ) = delete; \
 CLASS & operator=( const CLASS __VA_ARGS__ & ) = delete; \
 CLASS ( CLASS __VA_ARGS__ && ) = delete; \
 CLASS & operator=( CLASS __VA_ARGS__ && ) = delete
+
+#define CLASS_DEFAULT_COPY(CLASS, ...) \
+CLASS ( CLASS __VA_ARGS__ & ) = default; \
+CLASS & operator=( const CLASS __VA_ARGS__ & ) = default
+
+#define CLASS_DEFAULT_MOVE(CLASS, ...) \
+CLASS ( CLASS __VA_ARGS__ && ) = default; \
+CLASS & operator=( CLASS __VA_ARGS__ && ) = default
+
+#define CLASS_DEFAULT_COPY_MOVE(CLASS, ...) \
+CLASS_DEFAULT_COPY(CLASS, __VA_ARGS__); \
+CLASS_DEFAULT_MOVE(CLASS, __VA_ARGS__)
