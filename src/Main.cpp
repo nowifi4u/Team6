7f122da8a9cfa9944a34a2ca59b7d317b69ce9b3
Maxim Shamber <max.shamber@gmail.com>
2020-12-04 15:59:36 +0300

    Make GameData parsers
    + Link GameData.h types to Types.h
    + Add comments

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,5 @@
-#include "graph.h"
-#include "KKSCoordsCalculator.h"
+#include "GameData.h"
+
 #include <iostream>
 #include <iomanip>
 #include <memory>
@@ -10,25 +10,22 @@
 
 int main()
 {
-	GraphIdx g;
+	GameData gamedata;
 
-	std::cout << "Importing Graph..." << std::endl;
+	std::cout << "Importing GameData..." << std::endl;
 
-	readJSON_L0_file(g, "small_graph.json");
+	readJSON_L0_file(gamedata, "small_graph.json");
 	
 	std::cout << "Calculating coordinates..." << std::endl;
 
-	KKSCoordsCalculator b;
-	b.calculate(g.graph, 200, 200, 3);
-
 	std::cout << "Drawing Graph with coordinates..." << std::endl;
 
-	g.for_each_vertex([&g, &b](auto v) {
-		const auto& vertex = g.graph[v];
+	gamedata.graph.for_each_vertex([&](auto v) {
+		const auto& vertex = gamedata.graph.graph[v];
 		std::cout << v
 			<< " idx:" << vertex.idx
 			<< " length:" << (int32_t)vertex.post_idx
-			<< " pos:[" << b[v][0] << ' ' << b[v][1] << ']'
+			<< " pos:[" << vertex.pos_x << ' ' << vertex.pos_y << ']'
 			<< std::endl;
 		});
 
