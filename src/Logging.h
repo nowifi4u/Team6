3680d0c29ed3e6f4eee8162bc5e909095fc963c3
Maxim Shamber <max.shamber@gmail.com>
2020-12-04 14:06:25 +0300

    Add Logging.h

---
+++ src/Logging.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+
+#ifdef LOG_LEVEL_3
+#define LOG_LEVEL_2
+#endif
+
+#ifdef LOG_LEVEL_2
+#define LOG_LEVEL_1
+#endif
+
+
+#ifdef LOG_LEVEL_1
+#define LOG_1(args) std::cout << "log_1:" << args << std::endl
+
+#else
+#define LOG_1(args)
+
+#endif
+
+#ifdef LOG_LEVEL_2
+#define LOG_2(args) std::cout << "log_2:" << args << std::endl
+
+#else
+#define LOG_2(args)
+
+#endif
+
+#ifdef LOG_LEVEL_3
+#define LOG_3(args) std::cout << "log_3:" << args << std::endl
+
+#else
+#define LOG_3(args)
+
+#endif
