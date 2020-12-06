7679dc84fdedb4b91f59b9eda879917750a6a1a5
Maxim Shamber <max.shamber@gmail.com>
2020-12-06 21:21:59 +0300

    Test game_connector

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,53 @@
 #include "GameData.h"
 
+#include "packet-parser.h"
+#include "bincharstream.h"
+#include "binstream.h"
+
 #include <iostream>
 #include <iomanip>
-#include <memory>
-#include <SFML/Graphics.hpp>
+#include <algorithm>
 
+#include "game_connector.h"
 
-#include <iomanip>
+namespace
+{
+	bool keepGoing = true;
+	void shutdown(int) { keepGoing = false; }
+}
 
 int main()
 {
-	GameData gamedata;
+	boost::asio::io_service io;
+	
+	game_connector connector(io);
+	connector.connect("wgforge-srv.wargaming.net", "443");
+	
+	connector.socket.set_option(tcp::no_delay(true));
+	connector.socket.set_option(boost::asio::socket_base::receive_buffer_size(65536));
+	connector.socket.set_option(boost::asio::socket_base::send_buffer_size(65536));
+
+	std::wcout << "Sending Login request..." << std::endl;
+	connector.send(Packets::encode::Games::encodeJSON());
+	std::cout << "OK" << std::endl;
+
+	std::cout << "Waiting for answer..." << std::endl;
+	connector.wait_read();
+	std::cout << "OK" << std::endl;
+
+
+	auto response = connector.read_packet();
+	std::cout << "------------------------- BEGIN -------------------------" << std::endl;
+	std::cout << "Action: " << response.first << std::endl;
+	std::cout << response.second << std::endl;
+	std::cout << "-------------------------  END  -------------------------" << std::endl;
+
+	return 0;
+
+
+
+
+	/*GameData gamedata;
 
 	std::cout << "Importing GameData..." << std::endl;
 
@@ -29,7 +66,7 @@ int main()
 			<< " length:" << (int32_t)vertex.post_idx
 			<< " pos:[" << vertex.pos_x << ' ' << vertex.pos_y << ']'
 			<< std::endl;
-		});
+		});*/
 
 	return 0;
 }
