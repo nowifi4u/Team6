0cad9eb2ba4e9b924d0df8dfcc99791bf5872e83
Maxim Shamber <max.shamber@gmail.com>
2020-12-20 18:56:56 +0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,108 +5,35 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-#include "game_connector.h"
-#include "game_drawer.h"
+#include "game.h"
 
 int main()
 {
 	boost::asio::io_service io;
 	game_connector connector(io);
-	GameData gamedata;
 
 	try
 	{
 
-		connector.connect("wgforge-srv.wargaming.net", "443");
+		Game game(io);
 
-		//connector.socket.set_option(tcp::no_delay(true));
-		//connector.socket.set_option(boost::asio::socket_base::receive_buffer_size(65536));
-		//connector.socket.set_option(boost::asio::socket_base::send_buffer_size(65536));
+		game.init_gamedata("wgforge-srv.wargaming.net", "443", { "test3", "test3", "Game of Thrones", -1, 3 });
 
-		{
-			LOG_1("Sending Games request...");
-			connector.send_Games();
-
-			const auto response = connector.read_packet();
-		}
-
-		{
-			LOG_1("Sending Login request...");
-			connector.send_Login({ "test3", "test3", "Game of Thrones", -1, 3 });
-
-			const auto response = connector.read_packet();
-
-			GameData::readJSON_Login(gamedata, json::parse(response.second));
-		}
-
-		{
-			LOG_1("Sending L0 request...");
-			connector.send_Map({ 0 });
-
-			const auto response = connector.read_packet();
-
-			GameData::readJSON_L0(gamedata, json::parse(response.second));
-		}
-
-		{
-			LOG_1("Sending L10 request...");
-			connector.send_Map({ 10 });
-
-			const auto response = connector.read_packet();
-
-			GameData::readJSON_L10(gamedata, json::parse(response.second));
-		}
-
-		{
-			LOG_1("Sending L1 request...");
-			connector.send_Map({ 1 });
-
-			const auto response = connector.read_packet();
-
-			GameData::readJSON_L1(gamedata, json::parse(response.second));
-		}
-
-		{
-			LOG_1("Sending L1 request...");
-			connector.send_Map({ 1 });
-
-			const auto response = connector.read_packet();
-
-			GameData::readJSON_L1(gamedata, json::parse(response.second));
-		}
-
-		{
-			LOG_1("Sending Games request...");
-			connector.send_Games();
-
-			const auto response = connector.read_packet();
-		}
-
-		connector.disconnect();
-
-
-		game_drawer_config drawer_config;
-		game_drawer::status drawer_status = game_drawer::READY;
-
-		LOG_2("Creating game_drawer thread...");
-		boost::thread* drawer_thread = new boost::thread(&game_drawer_thread, boost::ref(gamedata), boost::ref(drawer_config), boost::ref(drawer_status));
+		game.drawer_start();
 
 
 		Sleep(3000);
-		drawer_status = game_drawer::UPDATING;
-		drawer_thread->interrupt();
+		game.drawer_set_state(game_drawer::UPDATING);
 
 		Sleep(1000);
-		drawer_status = game_drawer::CALCULATING;
-		drawer_thread->interrupt();
+		game.drawer_set_state(game_drawer::CALCULATING);
 
 		Sleep(1000);
-		drawer_status = game_drawer::READY;
-		drawer_thread->interrupt();
+		game.drawer_set_state(game_drawer::READY);
 
 
 		Sleep(1000);
-		//drawer_thread->join();
+		game.drawer_join();
 	}
 	catch (const nlohmann::detail::type_error& err)
 	{
