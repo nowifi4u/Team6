259f1ecbbc16684fb639ca1bbaab45f5774fbfe1
Maxim Shamber <max.shamber@gmail.com>
2020-12-24 00:08:05 +0300

    Refactor Coords storage

--- src/CoordsCalculator.h
+++ src/CoordsCalculator.h
@@ -1,54 +1,17 @@
 #pragma once
 
-#include "graph.h"
+#include "CoordsHolder.h"
 
-#include <boost/property_map/transform_value_property_map.hpp>
-#include <boost/graph/topology.hpp>
-
-
-
-class CoordsCalculator
+class CoordsCalculator : public CoordsHolder
 {
 public:
 
-	//------------------------------ TYPEDEFS ------------------------------//
+	CoordsCalculator() : CoordsHolder() {}
 
-	using topology_type = boost::rectangle_topology<>;
-	using point_type = topology_type::point_type;
-
-	using PositionVec = std::vector< point_type >;
-	using PositionMap = boost::iterator_property_map< PositionVec::iterator, boost::property_map< GraphIdx::Graph, boost::vertex_index_t >::type >;
-
-	//------------------------------ IMPL ------------------------------//
-
-protected:
-
-	PositionVec m_position_vec;
-	PositionMap m_position;
-
-public:
-
-	CoordsCalculator() = default;
+	CoordsCalculator(GraphIdx::Graph& g) : CoordsHolder(g) {}
 
 	virtual ~CoordsCalculator() = default;
 
 	virtual void calculate(GraphIdx::Graph& g, double topology_width, double topology_height, double unit_edge_length) = 0;
 
-	PositionMap& get_map()
-	{
-		return m_position;
-	}
-
-	point_type& operator[](GraphIdx::vertex_descriptor v)
-	{
-		return m_position[v];
-	}
-
-	const point_type& operator[](GraphIdx::vertex_descriptor v) const
-	{
-		return m_position[v];
-	}
 };
-
-
-
--- src/KKSCoordsCalculator.h
+++ src/KKSCoordsCalculator.h
@@ -52,26 +52,20 @@ public:
 
 	void calculate(GraphIdx::Graph& g, double topology_width, double topology_height, double unit_edge_length)
 	{
-		PositionVec position_vec(num_vertices(g));
-		PositionMap position(position_vec.begin(), get(boost::vertex_index, g));
+		CoordsHolder::init(g);
+
 		boost::minstd_rand gen;
 		topology_type topo(gen, 0, 0, topology_width, topology_width);
 
-		circle_graph_layout(g, position, std::min(topology_width, topology_height));
+		circle_graph_layout(g, m_position, std::min(topology_width, topology_height));
 
 		bool kamada = kamada_kawai_spring_layout(
 			g,
-			position,
+			m_position,
 			boost::make_transform_value_property_map(to_double(), get(&GraphIdx::EdgeProperties::length, g)),
 			topo,
 			boost::edge_length(unit_edge_length),
 			KamadaKawaiDone()
 		);
-
-
-		m_position_vec = std::move(position_vec);
-		m_position = boost::move(position);
 	}
 };
-
-
