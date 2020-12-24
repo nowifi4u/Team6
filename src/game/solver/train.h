4714dadb58fec82370d77b91bd492700871507da
Maxim Shamber <max.shamber@gmail.com>
2020-12-25 01:55:39 +0300

    Add TrainSolver

---
+++ src/game/solver/train.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "../data.h"
+class TrainSolver
+{
+public:
+
+	enum class State
+	{
+		EMERGENCY_FOOD,
+		EMERGENCY_ARMOR,
+		NORMAL_FOOD,
+		NORMAL_ARMOR,
+		RETURN
+	};
+
+	TrainSolver(const GameData& gamedata, Types::train_idx_t train_idx)
+		: gamedata(gamedata), train_idx(train_idx), gamedata_train(gamedata.players.at(gamedata.player_idx).trains.at(train_idx)) {}
+
+
+	Types::vertex_idx_t choose_target_NORMAL_FOOD() const
+	{
+		
+	}
+
+	Types::vertex_idx_t choose_target_NORMAL_ARMOR() const
+	{
+		
+	}
+
+	Types::vertex_idx_t choose_target_EMERGENCY_FOOD() const
+	{
+		
+	}
+
+	Types::vertex_idx_t choose_target_EMERGENCY_ARMOR() const
+	{
+		
+	}
+
+	Types::vertex_idx_t choose_target() const
+	{
+		switch (state)
+		{
+		case State::NORMAL_FOOD: return choose_target_NORMAL_FOOD();
+		case State::NORMAL_ARMOR: return choose_target_NORMAL_ARMOR();
+		case State::EMERGENCY_FOOD: return choose_target_EMERGENCY_FOOD();
+		case State::EMERGENCY_ARMOR: return choose_target_EMERGENCY_ARMOR();
+		case State::RETURN: return gamedata.home_idx;
+		}
+	}
+
+private:
+	const Types::train_idx_t train_idx;
+
+public:
+	const GameData& gamedata;
+	const Trains::Train& gamedata_train;
+
+	TrainSolver::State state;
+};
