a7827c05860d1a27165e8d08a002028ce10fc76a
Maxim Shamber <max.shamber@gmail.com>
2020-12-25 21:04:49 +0300

    Add namespace SpriteUtils & function certerOrigin

--- src/render/SpriteUtils.h
+++ src/render/SpriteUtils.h
@@ -2,7 +2,19 @@
 
 #include <SFML/Graphics.hpp>
 
-inline void setSize(sf::Sprite& sprite, const sf::Vector2f& size)
-{
-	sprite.setScale(size.x / sprite.getTexture()->getSize().x, size.y / sprite.getTexture()->getSize().y);
-}
+namespace SpriteUtils {
+
+	inline void setSize(sf::Sprite& sprite, const sf::Vector2f& size)
+	{
+		sprite.setScale(size.x / sprite.getTexture()->getSize().x, size.y / sprite.getTexture()->getSize().y);
+	}
+
+	inline void centerOrigin(sf::Sprite& sprite)
+	{
+		sprite.setOrigin(
+			sprite.getTexture()->getSize().x / 2.f,
+			sprite.getTexture()->getSize().y / 2.f
+		);
+	}
+
+} // SpriteUtils
