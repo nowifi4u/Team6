6a6e4cbf9def5bd68c0dfe601cf757da8e4efe38
Maxim Shamber <max.shamber@gmail.com>
2020-12-26 01:53:00 +0300

    Update CoordsHolder to use GraphVertexMap

--- src/graph/CoordsHolder.h
+++ src/graph/CoordsHolder.h
@@ -5,9 +5,9 @@
 #include <boost/property_map/transform_value_property_map.hpp>
 #include <boost/graph/topology.hpp>
 
+#include "GraphVertexMap.h"
 
-
-class CoordsHolder
+class CoordsHolder : public GraphVertexMap<typename boost::rectangle_topology<>::point_type>
 {
 public:
 
@@ -16,65 +16,14 @@ public:
 	using topology_type = boost::rectangle_topology<>;
 	using point_type = topology_type::point_type;
 
-	using PositionVec = std::vector<point_type>;
-	using PositionMap = boost::iterator_property_map< PositionVec::iterator, boost::property_map<GraphIdx::Graph, boost::vertex_index_t>::type>;
-
 	//------------------------------ IMPL ------------------------------//
 
-protected:
-
-	PositionVec m_position_vec;
-	PositionMap m_position;
 
 public:
 
-	CoordsHolder() = default;
+	CoordsHolder() : GraphVertexMap() {}
 
-	CoordsHolder(GraphIdx::Graph& g)
-	{
-		init(g);
-	}
-
-	void init(GraphIdx::Graph& g)
-	{
-		m_position_vec = PositionVec(num_vertices(g));
-		m_position = PositionMap(m_position_vec.begin(), get(boost::vertex_index, g));
-	}
+	CoordsHolder(const GraphIdx::Graph& g) : GraphVertexMap(g) {}
 
 	virtual ~CoordsHolder() = default;
-
-	PositionMap& get_map()
-	{
-		return m_position;
-	}
-
-	point_type& operator[](GraphIdx::vertex_descriptor v)
-	{
-		return m_position[v];
-	}
-
-	const point_type& operator[](GraphIdx::vertex_descriptor v) const
-	{
-		return m_position[v];
-	}
-
-	template <class Func>
-	void for_each(GraphIdx::Graph& g, Func f)
-	{
-		GraphIdx::vertex_iterator vi, vend;
-		for (boost::tie(vi, vend) = boost::vertices(g); vi != vend; ++vi)
-		{
-			f(m_position[*vi]);
-		}
-	}
-
-	template <class Func>
-	void for_each(const GraphIdx::Graph& g, Func f) const
-	{
-		GraphIdx::vertex_iterator vi, vend;
-		for (boost::tie(vi, vend) = boost::vertices(g); vi != vend; ++vi)
-		{
-			f(m_position[*vi]);
-		}
-	}
 };
