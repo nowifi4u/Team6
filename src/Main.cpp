d22924e2b1c4fcf91d5d3199be390bd15d99dd78
enid3 <includev5@gmail.com>
2020-12-25 13:35:31 +0300

    Add spdlog initialization to Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,10 +5,18 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
+
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#include <spdlog/spdlog.h>
+
 #include <src/game.h>
+
 
 int main()
 {
+    spdlog::set_pattern("[%H:%M:%S:%e] [%^%l%$] [thread %t] [%@::%!] %v");
+    spdlog::set_level(spdlog::level::trace);
+
 	boost::asio::io_service io;
 	server_connector connector(io);
 
@@ -25,11 +33,11 @@ int main()
 	}
 	catch (const nlohmann::detail::type_error& err)
 	{
-		std::cout << "ERROR! " << err.what() << std::endl;
+	    SPDLOG_ERROR(err.what());
 	}
 	catch (const std::runtime_error& err)
 	{
-		std::cout << "ERROR! " << err.what() << std::endl;
+        SPDLOG_ERROR(err.what());
 	}
 
 	//connector.disconnect();
