2c8490221581c03bc1adead3f4479e6666235984
Maxim Shamber <max.shamber@gmail.com>
2020-12-25 17:55:23 +0300

    Create readable_only_pmap.h

---
+++ src/graph/readable_only_pmap.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace boost {
+    namespace detail {
+        template <typename Map>
+        struct readable_only_pmap : public Map {
+            readable_only_pmap(Map map) : Map(map) { }
+
+            // overrule the category tag
+            typedef boost::readable_property_map_tag category;
+        };
+    }
+}
