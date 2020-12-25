7be4ee8d7de42cf45b37a6968dcd134601903985
Maxim Shamber <max.shamber@gmail.com>
2020-12-26 02:44:25 +0300

    Fix GraphDijkstra being const

--- src/game/solver/train.h
+++ src/game/solver/train.h
@@ -17,7 +17,7 @@ public:
 		STANDBY
 	};
 
-	TrainSolver(const GameData& gamedata, const GraphDijkstra& graphsolver, Types::train_idx_t train_idx)
+	TrainSolver(GameData& gamedata, const GraphDijkstra& graphsolver, Types::train_idx_t train_idx)
 		: gamedata(gamedata), 
 		graphsolver(graphsolver),
 		train_idx(train_idx), gamedata_train(gamedata.players.at(gamedata.player_idx).trains.at(train_idx)) {}
@@ -63,7 +63,7 @@ public:
 	const GameData& gamedata;
 	const Trains::Train& gamedata_train;
 
-	const GraphDijkstra& graphsolver;
+	GraphDijkstra& graphsolver;
 
 	TrainSolver::State state;
 };
