b9bafbc50a857deab469fbb391c04340ed996fc5
enid3 <includev5@gmail.com>
2020-12-25 14:32:53 +0300

    spdlog integration in game/data/event.h

--- src/game/data/event.h
+++ src/game/data/event.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "Types.h"
-#include "utils/Logging.h"
 
 #include <nlohmann/json.hpp>
 #include <boost/ptr_container/ptr_vector.hpp>
+#include <spdlog/spdlog.h>
 
 
 namespace Events {
@@ -159,7 +159,7 @@ namespace Events {
 	{
 		EventType event_type = (EventType)j["type"].get<int>();
 
-		LOG_1("game_data::make_Event: " << j);
+		SPDLOG_DEBUG("game_data::make_Event: {} ", j);
 
 		switch (event_type)
 		{
