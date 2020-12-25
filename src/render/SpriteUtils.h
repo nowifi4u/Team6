e0f4333c97153d3ad2501cc05a49e22f48904d27
Maxim Shamber <max.shamber@gmail.com>
2020-12-25 21:46:31 +0300

    Add getSize & intergate into other functions

--- src/render/SpriteUtils.h
+++ src/render/SpriteUtils.h
@@ -4,16 +4,25 @@
 
 namespace SpriteUtils {
 
+	inline sf::Vector2f getSize(const sf::Sprite& sprite)
+	{
+		return sf::Vector2f(sprite.getTexture()->getSize().x, sprite.getTexture()->getSize().y);
+	}
+
 	inline void setSize(sf::Sprite& sprite, const sf::Vector2f& size)
 	{
-		sprite.setScale(size.x / sprite.getTexture()->getSize().x, size.y / sprite.getTexture()->getSize().y);
+		const auto act_size = getSize(sprite);
+
+		sprite.setScale(size.x / act_size.x, size.y / act_size.y);
 	}
 
 	inline void centerOrigin(sf::Sprite& sprite)
 	{
+		const auto act_size = getSize(sprite);
+
 		sprite.setOrigin(
-			sprite.getTexture()->getSize().x / 2.f,
-			sprite.getTexture()->getSize().y / 2.f
+			act_size.x / 2.f,
+			act_size.y / 2.f
 		);
 	}
 
