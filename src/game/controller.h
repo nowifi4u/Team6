eeafc4537de9fbe17ad3c42a388aaa7e3db9aef5
enid3 <includev5@gmail.com>
2020-12-26 16:09:14 +0300

    Move not logically reasonable functions  from Game to GameController
    Move responobility of doing game-related actions(e.g move train, connect to server)  from game.h::Game to game/controller.h::GameController.
    So now:
    Game -- resposible for starting main cycle
    GameController --  for hadling game-related actions(e.g. move Train, create new Game).
    Drawer -- for drawing all staff.

--- src/game/controller.h
+++ src/game/controller.h
@@ -5,8 +5,109 @@
 #ifndef TEAM6_CONTROLLER_H
 #define TEAM6_CONTROLLER_H
 
+#include "utils/network/server_connector.h"
+
 class GameController
 {
+private:
+    GameData& gamedata;
+    server_connector connector;
+
+public:
+    GameController(GameData& gamedata, boost::asio::io_service& io)
+        : gamedata(gamedata), connector(io)
+    {
+        SPDLOG_DEBUG("constructing from io.");
+    }
+
+
+    void init(const server_connector::Login& lobby)
+    {
+        SPDLOG_DEBUG("initiating game controller.");
+
+        {
+            SPDLOG_TRACE("sending Login request...");
+            connector.send_Login(lobby);
+
+            const auto response = connector.read_packet();
+            SPDLOG_TRACE("received response code: {}", response.first);
+
+            GameData::readJSON_Login(gamedata, json::parse(response.second));
+        }
+
+        {
+            SPDLOG_TRACE("sending L0 request...");
+            connector.send_Map({ 0 });
+
+            const auto response = connector.read_packet();
+            SPDLOG_TRACE("received response code: {}", response.first);
+
+            GameData::readJSON_L0(gamedata, json::parse(response.second));
+        }
+
+        {
+            SPDLOG_TRACE("sending L10 request...");
+            connector.send_Map({ 10 });
+
+            const auto response = connector.read_packet();
+            SPDLOG_TRACE("received response code: {}", response.first);
+
+            GameData::readJSON_L10(gamedata, json::parse(response.second));
+        }
+
+        {
+            SPDLOG_TRACE("sending L1 request...");
+            connector.send_Map({ 1 });
+
+            const auto response = connector.read_packet();
+            SPDLOG_TRACE("received response code: {}", response.first);
+
+            GameData::readJSON_L1(gamedata, json::parse(response.second));
+        }
+
+    }
+
+    void connect(const std::string& addr, const std::string& port)
+    {
+        SPDLOG_INFO("Connecting to {}:{}", addr, port);
+        connector.connect(addr, port);
+    }
+
+    void update()
+    {
+        SPDLOG_DEBUG("updating game data...");
+        connector.send_Map({ 1 });
+
+        const auto response = connector.read_packet();
+        SPDLOG_TRACE("received response code: {}", response.first);
+
+        GameData::readJSON_L1(gamedata, json::parse(response.second));
+    }
+    void reset()
+    {
+        SPDLOG_DEBUG("resetting controller.");
+
+        connector.disconnect();
+        gamedata.clear();
+        SPDLOG_DEBUG("resetting controller end.");
+    }
+
+    void await_run()
+    {
+        SPDLOG_DEBUG("sending turn package...");
+        this->connector.send_Turn();
+        SPDLOG_DEBUG("waiting for turn package answer...");
+        this->connector.read_packet();
+    }
+
+    void await_move()
+    {
+        SPDLOG_DEBUG("sending turn package...");
+        this->connector.send_Turn();
+        SPDLOG_DEBUG("waiting for turn package answer...");
+        this->connector.read_packet();
+    }
+
 
 };
 
