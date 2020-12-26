176657e1a75a25e9d40180c0b7e0cc6fa6808ea1
Maxim Shamber <max.shamber@gmail.com>
2020-12-26 19:56:55 +0300

    Add distance_to and calculate_Move

--- src/game/solver/path.h
+++ src/game/solver/path.h
@@ -21,19 +21,69 @@ public:
 		graphsolver.calculate(e, pos);
 	}
 
-	std::optional<server_connector::Move> calculate_Move(GraphIdx::vertex_descriptor target)
+	Types::edge_length_t distance_to(GraphIdx::vertex_descriptor target)
 	{
-		auto solver = graphsolver.get_obj(target);
-		auto path = graphsolver.get_path(target);
+		return graphsolver[target].first;
 	}
 
-public:
+	std::optional<server_connector::Move> calculate_Move(GraphIdx::edge_descriptor epos, Types::edge_idx_t pos, Types::train_idx_t train_idx, GraphIdx::vertex_descriptor target)
+	{
+		if (target == gamedata.graph().null_vertex()) return std::nullopt;
 
-	const GameData& gamedata;
-	GraphEdgeDijkstra& graphsolver;
+		const auto solver = graphsolver.get_obj(target);
+		GraphDijkstra::path_t path = graphsolver.get_path(target);
 
+		const Trains::Train& train_data = gamedata.self_data().trains.at(train_idx);
+
+		if (solver.second == false)
+		{
+			GraphIdx::vertex_descriptor v = boost::source(epos, gamedata.graph());
+
+			if (train_data.position == 0)
+			{
+				if (path.size() == 0) return std::nullopt;
+
+				if (Graph::isSource(gamedata.graph(), v, path.front()))
+				{
+					return server_connector::Move{ Graph::get_edge_props(gamedata.graph(), v, path.front())->idx, 1, train_idx };
+				}
+				else
+				{
+					return server_connector::Move{ Graph::get_edge_props(gamedata.graph(), v, path.front())->idx, -1, train_idx };
+				}
+			}
+			else
+			{
+				return server_connector::Move{ gamedata.graph()[epos].idx, -1, train_idx };
+			}
+		}
+		else
+		{
+			GraphIdx::vertex_descriptor v = boost::target(epos, gamedata.graph());
+
+
+			if (train_data.position == gamedata.graph()[epos].length)
+			{
+				if (path.size() == 0) return std::nullopt;
+
+				if (Graph::isSource(gamedata.graph(), v, path.front()))
+				{
+					return server_connector::Move{ Graph::get_edge_props(gamedata.graph(), v, path.front())->idx, 1, train_idx };
+				}
+				else
+				{
+					return server_connector::Move{ Graph::get_edge_props(gamedata.graph(), v, path.front())->idx, -1, train_idx };
+				}
+			}
+			else
+			{
+				return server_connector::Move{ gamedata.graph()[epos].idx, 1, train_idx };
+			}
+		}
+	}
 
 protected:
 
-
+	const GameData& gamedata;
+	GraphEdgeDijkstra& graphsolver;
 };
