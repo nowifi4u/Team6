53bbf4d83b853f2aea3c547957f51dd2fb839b5c
enid3 <includev5@gmail.com>
2020-12-27 13:06:22 +0300

    impliment doprint for Event_GameOver

--- src/game/data/event.h
+++ src/game/data/event.h
@@ -221,6 +221,14 @@ namespace Events {
 			return event;
 		}
 
+        virtual std::basic_ostream<char>& doprint(std::basic_ostream<char> &os) const
+        {
+            os  << "[Event GameOver ";
+            os << ']';
+
+            return os;
+        }
+
 		CLASS_VIRTUAL_DESTRUCTOR(Event_GameOver);
 	};
 
