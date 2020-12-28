04db8f898d8610defd33c042b37dd436d12535df
Maxim Shamber <max.shamber@gmail.com>
2020-12-28 15:03:05 +0300

    Add ability to restrict edges

--- src/game/solver/graph_edge.h
+++ src/game/solver/graph_edge.h
@@ -10,8 +10,8 @@ class GraphEdgeDijkstra
 
 public:
 
-	GraphEdgeDijkstra(const Graph::Graph& graph)
-		: s(graph), t(graph) {}
+	GraphEdgeDijkstra(const Graph::Graph& graph, const GraphDijkstra::weightmap_transform_t& weightmap_transform)
+		: s(graph, weightmap_transform), t(graph, weightmap_transform) {}
 
 	void calculate(Graph::edge_descriptor e, Types::edge_idx_t pos)
 	{
